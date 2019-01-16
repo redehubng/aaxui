ce1cd08c1dd13f322f42765bb7c9f3eaddaedf5d
solinkz <solomoneyitene@gmail.com>
2019-01-16 23:30:23 +0100

    submenu initial and search result display initial

--- js/main.js
+++ js/main.js
@@ -29,9 +29,56 @@
             $buttomBtnToggler.click(function () {
                 $('#buttom-nav').append($buttomNav),
                 $('#content').toggleClass('active-buttom'),
-                $('#sidebar').toggleClass('active-buttom'),
+                $('#sidebar').toggleClass('active-buttom'),0
                 $buttomNav.find('.nav-avatar').hide();
                 $buttomNav.toggleClass('active');
             });
             
+
+    // SEARCH SCRIPT
+            let $searchForm = $('.search-form');
+            let $searchComponents = $('.search-component');
+            let $ssDisplay = $('.search-select-display');
+            let $filterItem = $('.filter .dropdown-item');
+            // show the search-components
+           
+            
+            function openAction() {
+                $searchComponents.show().css('z-index', '3'),
+                $ssDisplay.css('opacity', '1');
+                $('.crumb').hide();
+
+            }
+
+            function closeAction() {
+                $searchComponents.hide().css('z-index', '-10'),
+                $ssDisplay.css('opacity', '0');
+                $('.crumb').show();
+
+            }
+
+            $searchForm.keypress(openAction);
+          
+            $searchForm.keyup(function(){
+                if($searchForm.val() == ""){
+                   closeAction();
+                }
+
+            });
+
+
+            // DISPLAY FILTER LIST AND TYPE
+            
+                
+            if ($filterItem.hasClass('active')){
+                $('.filter-type').text($('.filter .dropdown-item.active').text());
+            }
+            $filterItem.click(function () {
+               $filterItem.removeClass('active');
+               $(this).addClass('active');  
+               
+               let filterText = $(this).text();
+               $('.filter-type').text(filterText);
+            })
+
 });
